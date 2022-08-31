5e6e5363507290032a90425073a8d6596c82e6a7
Rosanakthar <rosanakthar21@gmail.com>
2022-08-31 22:30:24 +0530

    setup background

---
+++ imgur/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Component/Nav';
+
+function App() {
+  return (
+    <div classNameName="App">
+      {/* navigation bar */}
+      <Navigation></Navigation>
+      <br />
+      <h1>gggg</h1>
+
+    </div>
+  );
+}
+
+export default App;
